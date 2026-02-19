2b02a577f3e2d73917723cdc0a55eff2bb8ddec7
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2026-02-19 20:12:40 +0100

    feat: init shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Inter } from "next/font/google";
 
 export const metadata: Metadata = {
 	title: "Create T3 App",
@@ -9,16 +9,16 @@ export const metadata: Metadata = {
 	icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-const geist = Geist({
+const inter = Inter({
 	subsets: ["latin"],
-	variable: "--font-geist-sans",
+	variable: "--font-inter",
 });
 
 export default function RootLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<html className={`${geist.variable}`} lang="en">
+		<html className={`${inter.variable}`} lang="en">
 			<body>{children}</body>
 		</html>
 	);
